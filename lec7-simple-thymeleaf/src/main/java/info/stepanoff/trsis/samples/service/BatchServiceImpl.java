8b6a75593c2d078dfe39c8dcd8972eb757a5aa08
PavelStepanov <wildpierre@gmail.com>
2021-10-20 18:57:54 +0300

    fix for keystore location

--- lec7-simple-thymeleaf/src/main/java/info/stepanoff/trsis/samples/service/BatchServiceImpl.java
+++ lec7-simple-thymeleaf/src/main/java/info/stepanoff/trsis/samples/service/BatchServiceImpl.java
@@ -1,16 +1,15 @@
 package info.stepanoff.trsis.samples.service;
 
 import info.stepanoff.trsis.samples.db.dao.BatchRepository;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import info.stepanoff.trsis.samples.db.model.Batch;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class BatchServiceImpl implements BatchService {
-
-    private final static Logger log = Logger.getLogger(SchoolServiceImpl.class);
 
     @Autowired
     private BatchRepository batchRepository;
@@ -19,9 +18,9 @@ public class BatchServiceImpl implements BatchService {
     public Iterable<Batch> findBySchool(Integer school) {
         return batchRepository.findBySchool(school);
     }
-    
+
     @Override
-    public void save(Batch batch){
+    public void save(Batch batch) {
         batchRepository.save(batch);
     }
 
